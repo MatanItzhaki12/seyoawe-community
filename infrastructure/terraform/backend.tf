7834242ae6584ff3e1e934cb87ec199870bbde08
Matan <mtmt8520@gmail.com>
2026-04-15 21:17:38 +0300

    Update Terraform configuration to change AWS region and backend settings
    - Modified the AWS region from us-east-1 to us-east-2 in backend.tf and variables.tf.
    - Updated backend configuration to use a lockfile instead of a DynamoDB table for state locking.

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "seyoawe-terraform-state-bucket-8520"
     key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     encrypt        = true
-    dynamodb_table = "seyoawe-terraform-lock"
+    use_lockfile   = true
   }
 }
